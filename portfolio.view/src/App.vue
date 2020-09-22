d60cd287f7cc8c00fea99dd14fff25995315e81e
Naendo <muellvalle@gmail.com>
2020-09-22 16:30:52 +0200

    setup infrastructure

--- portfolio.view/src/App.vue
+++ portfolio.view/src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
+<style lang="scss">
+@import "assets/styles/scss/global.scss";
+
 #app {
+  background-color: $test;
+
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
